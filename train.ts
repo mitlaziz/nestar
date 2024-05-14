502d44d0333f6594022a879a8947fa1f6ed709cb
Max <lazizyuldoshev93@gmail.com>
2024-05-14 22:35:35 +0900

    fix: modify nest cli detail

--- train.ts
+++ train.ts
@@ -1,4 +1,4 @@
-// MIT TASK ZK
+// MIT TASK ZL
 /**Shunday function yozing, u parametrda berilgan stringni 
  * kebab casega otkazib qaytarsin. Bosh harflarni kichik harflarga ham otkazsin.
 MASALAN: stringToKebab(“I love Kebab”) return “i-love-kebab”
